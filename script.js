a54aa13ac42548055b54e65217f342437cc472a3
DaniDPX <tech@danidpx.com>
2020-08-27 20:11:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,30 +21,63 @@
 //   };
 
 
-window.onload = function() {
-        // init();
-        // doSomethingElse();
-        console.log("This is from Window.onLoad");
-    
-      };
+
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("para").innerText = "This is a new paragraph";
-    console.log("Paragraph text changed!");
+    DOMmodify();
+    gebeya();
   });
 
 
+  window.onload = function() { // same as window.addEventListener('load', (event) => {
+    console.log("This is from Window.onLoad");
+
+  };
+
+
+function DOMmodify()
+{
+    document.getElementById("para").innerText = "This is a new paragraph";
+    console.log("Paragraph text changed!");
+}
 
 
 
 
-//   window.onload = function() { // same as window.addEventListener('load', (event) => {
-//     alert('Page loaded');
-
-//     // image is loaded at this time
-//     alert(`Image size: ${img.offsetWidth}x${img.offsetHeight}`);
-//   };
 
 
+
+
+
+gebeya = () => {
+    console.log("This is an arrow function");
+}
+
+function gebeya(){
+
+
+}
+
+
+
+
+
+
+
+
+
+setInterval(() => {
+    console.log(document.getElementById("para").style.display);
+
+    if (document.getElementById("para").style.display == "none")
+    {
+        document.getElementById("para").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("para").style.display = "none";
+    }
+    
+}, 3000);
